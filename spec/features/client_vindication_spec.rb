ffadb1c3d29dc1e20c8b1e21960692a4b2ff560c
Marcin Styszewski <marcin.styszewski@daftcode.pl>
2016-04-29 11:12:20 +0200

    kruczek to pedal

---
+++ spec/features/client_vindication_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+feature 'admin can vindicate user' do
+
+  scenario 'admin can visit client payments from user index' do
+    visit clients_path
+  end
+
+  scenario 'admin can vindincate client' do
+  end
+
+end
