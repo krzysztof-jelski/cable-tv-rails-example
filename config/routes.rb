bf2f0b74ea69c06c2830f7709bb90187492493ea
Małgorzata Adamczyk <Malgorzata@MacBook-Air-Magorzata.local>
2016-04-29 12:11:21 +0200

    visit client payments

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  resources :clients
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'clients#index'
-  resources :payments
+
+  resources :clients do
+    resources :payments
+  end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
--- spec/features/client_vindication_spec.rb
+++ spec/features/client_vindication_spec.rb
@@ -1,16 +1,18 @@
 require 'rails_helper'
 
 feature 'admin can vindicate user' do
+  let!(:client) { create(:client) }
 
   scenario 'admin can visit client payments from user index' do
     visit clients_path
     click_link 'Payments'
-    expect(page).to have_css('header h1', text: 'Payments')
+    expect(page).to have_css('h1', text: 'Payments')
   end
 
   scenario 'admin can vindincate client' do
-    # visit 'clients/1/payments'
-
+    visit client_payments_path(client)
+    click_link 'Vindicate'
+    expect(page).to 
   end
 
 end
