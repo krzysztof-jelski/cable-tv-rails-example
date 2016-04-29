d6b3c9918d94ad051670e9174557c07f8cd6fc10
Małgorzata Adamczyk <Malgorzata@MacBook-Air-Magorzata.local>
2016-04-29 11:24:40 +0200

    first spec

--- spec/features/client_vindication_spec.rb
+++ spec/features/client_vindication_spec.rb
@@ -4,6 +4,8 @@ feature 'admin can vindicate user' do
 
   scenario 'admin can visit client payments from user index' do
     visit clients_path
+    click_link 'Payments'
+    expect(page).to have_css('header h1', text: 'Payments')
   end
 
   scenario 'admin can vindincate client' do
